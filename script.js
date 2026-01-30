442b9a020c934cb859f9e53e2eb0b03428f07c51
Alessandro Sforza <137790980+alessandrosforza@users.noreply.github.com>
2026-01-30 22:53:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const contactBtn = document.getElementById('contactBtn');
-const email = document.getElementById('email');
-
-contactBtn.addEventListener('click', () => {
-  email.classList.toggle('hidden');
-});
